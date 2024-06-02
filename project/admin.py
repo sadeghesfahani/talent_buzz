24324ae75a7dd1bd1fe3cd6d3c6bb2e43ad78e79
Sina <sadeghesfahani.sina@gmail.com>
2024-06-02 13:18:27 +0200

    add hours to admin panel

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import Gig
+
+from .models import Gig, Project, ProjectReport, GigReport
 
 # Register your models here.
 admin.site.register(Gig)
+admin.site.register(Project)
+admin.site.register(ProjectReport)
+
+
+class GigReportAdmin(admin.ModelAdmin):
+    list_display = (
+    'freelancer', 'gig', 'submitted_at', 'start_time', 'end_time', 'status', 'reviewed_by', 'hours_spent')
+    readonly_fields = ('hours_spent','submitted_at')
+    fields = (
+    'freelancer', 'gig', 'document', 'text', 'start_time', 'end_time', 'status', 'reviewed_by',
+    'review', 'hours_spent')
+    def hours_spent(self, obj):
+        delta = obj.hours_spent
+        return f"{delta.total_seconds() / 3600:.2f} hours"
+
+    hours_spent.short_description = 'Hours Spent'
+
+
+admin.site.register(GigReport, GigReportAdmin)
